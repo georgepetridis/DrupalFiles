570ccfd217e506b3685bed983a78ff1e83bb4a99
George Petridis <george@georgepetridis.com>
2015-10-13 21:36:30 -0400

    Update template.php

--- template.php
+++ template.php
@@ -1,12 +1,12 @@
 <?php
 
 // Removes the generator tag in the document head.
-function braxon_html_head_alter(&$head_elements) {
+function THEME_html_head_alter(&$head_elements) {
     unset($head_elements['system_meta_generator']);
 }
 
 // Allows the block to be rendered in page.tpl.php.
-function block_render($module, $block_id) {
+function THEME_render($module, $block_id) {
  	$block = block_load($module, $block_id);
  	$block_content = _block_render_blocks(array($block));
   	$build = _block_get_renderable_array($block_content);
@@ -15,7 +15,7 @@ function block_render($module, $block_id) {
 }
 
 // Changes the default behavior of the language display so the acronym is displayed instead of the full language name.
-function braxon_links__locale_block(&$vars) {
+function THEME_links__locale_block(&$vars) {
   foreach($vars['links'] as $language => $langInfo) {
     $abbr = $langInfo['language']->language;
     $name = $langInfo['language']->native;
